5a4f7c5f1ffb194fcbe692aee3e33e2e06b3c267
Harsh Gupta <harshgupta2204@live.com>
2016-11-04 22:44:51 -0400

    Fixed the dice code for the animation and multiple dices :)

--- src/monopolyalpha/Dice.java
+++ src/monopolyalpha/Dice.java
@@ -5,45 +5,118 @@
 package monopolyalpha;
 
 import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.Random;
 import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
 
 /**
  *
  * @author 714449
  */
 public class Dice {
-     public static void sleep(int amt) {
+
+    public static void sleep(int amt) {
         long a = System.currentTimeMillis();
         long b = System.currentTimeMillis();
         while ((b - a) <= amt) {
             b = System.currentTimeMillis();
         }
     }
-    public static int rollDice() {
-        int roll=0;
-        JDialog dice = new JDialog();
-        JLabel di = new JLabel(new ImageIcon(ThemeSelect.dicePath +"Dice 1.png"));
-        dice.add(di);
-        dice.setSize(di.getWidth(),di.getHeight());
-        dice.setUndecorated(true);
-        dice.getRootPane().setOpaque(false);
-        dice.getContentPane().setBackground(new Color (0, 0, 0, 0));
-        dice.setBackground(new Color (0, 0, 0, 0));
-        dice.pack();
-        dice.setLocationRelativeTo(null);
-        dice.setVisible(true);
-        for (int i = 0; i < 9; i++) {
-            di.setIcon(null);
-            sleep(100);
-            Random r = new Random();
-            int randomNumber = r.nextInt(6) + 1;
-            di.setIcon(new ImageIcon(ThemeSelect.dicePath + randomNumber + ".png"));
-            di.repaint();
-            roll = randomNumber;
-            sleep(250);
+//REDUNDANT CUZ DOESNT WORK, MAYBE FIX LATER \/
+//    private static void blinkingDie(JDialog j) {
+//        for (int i = 0; i < 10; i++) {
+//            j.setVisible(false);
+//            sleep(200);
+//            j.setVisible(true);
+//        }
+//        j.setVisible(false);
+//    }
+
+    public static int rollDice(String theme, int num) {
+        int roll = 0;
+        String path = null;
+
+        switch (theme) {
+            case "Generic":
+                path = "Icons/Dice/Generic/Dice ";
+                break;
+            case "Canada":
+                path = "Icons/Dice/Maple/Dice ";
+                break;
+            case "Super":
+                path = "Icons/Dice/Superhero/Dice ";
+                break;
+        }
+        if (num == 1) {
+            JDialog dice = new JDialog();
+            dice.setAlwaysOnTop(true);
+            JLabel di = new JLabel(new ImageIcon(path + "1.png"));
+            dice.add(di);
+            dice.setSize(di.getWidth(), di.getHeight());
+            dice.setUndecorated(true);
+            dice.getRootPane().setOpaque(false);
+            dice.getContentPane().setBackground(new Color(0, 0, 0, 0));
+            dice.setBackground(new Color(0, 0, 0, 0));
+            dice.pack();
+            dice.setLocationRelativeTo(null);
+            dice.setVisible(true);
+            for (int i = 0; i < 9; i++) {
+                dice.setVisible(false);
+                sleep(100);
+                Random r = new Random();
+                int randomNumber = r.nextInt(6) + 1;
+                di.setIcon(new ImageIcon(path + randomNumber + ".png"));
+                dice.setVisible(true);
+                di.repaint();
+                if (i == 8) {
+                    roll = randomNumber;
+                }
+                sleep(500);
+            }
+//            blinkingDie(dice);
+        }
+        if (num == 2) {
+            JDialog dice = new JDialog();
+            dice.setAlwaysOnTop(true);
+            JLabel di1 = new JLabel(new ImageIcon(path + "1.png"));
+            JLabel di2 = new JLabel(new ImageIcon(path + "1.png"));
+            dice.setLayout(new GridLayout(1, 2));
+            dice.add(di1);
+            dice.add(di2);
+            dice.setSize(di1.getWidth() + di2.getWidth(), di1.getHeight() + di2.getHeight());
+            dice.setUndecorated(true);
+            dice.getRootPane().setOpaque(false);
+            dice.getContentPane().setBackground(new Color(0, 0, 0, 0));
+            dice.setBackground(new Color(0, 0, 0, 0));
+//        dice.setGlassPane(di);
+            dice.pack();
+            dice.setLocationRelativeTo(null);
+
+            dice.setVisible(true);
+            for (int i = 0; i < 9; i++) {
+                dice.setEnabled(false);
+                dice.setVisible(false);
+                sleep(100);
+                Random r1 = new Random();
+                Random r2 = new Random();
+                int randomNumber1 = r1.nextInt(6) + 1;
+                int randomNumber2 = r2.nextInt(6) + 1;
+                di1.setIcon(new ImageIcon(path + randomNumber1 + ".png"));
+                di2.setIcon(new ImageIcon(path + randomNumber2 + ".png"));
+                dice.setVisible(true);
+                di1.repaint();
+                di2.repaint();
+                if (i == 8) {
+                    roll = randomNumber1 + randomNumber2;
+                }
+                sleep(500);
+            }
+//            blinkingDie(dice);
         }
         return roll;
     }
