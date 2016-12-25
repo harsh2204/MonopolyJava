ec29b1ddb1c43860c288cb76ef9efc0a7df2a52c
Harsh Gupta <harshgupta2204@live.com>
2016-12-25 12:30:01 -0500

    Added comments

--- src/monopolyalpha/playerData.java
+++ src/monopolyalpha/playerData.java
@@ -7,14 +7,14 @@ package monopolyalpha;
 
 /**
  *
- * @author karmi
+ * @author Karmit, Harsh
  */
 public class playerData
   {
 
     public int numplayers, i,dice,chance;
     public String theme;
-    public int[] money, numprop, cpos, npos, bonus, jailfee;
+    public int[] money, numprop, cpos,/*what is this? >*/ npos,/*move bonus and jailfee to game class*/ bonus, jailfee;
     public String[] icon, name;
     //1D array used so that everything can be called individually
     //Although it might take more time and more code lines it will make it easy in future 
@@ -25,12 +25,12 @@ public class playerData
     public playerData()
       {
         numplayers = it.Players;
-        datatransfer();
-        playermovement();
+        datatransfer();//call this under Game class where we setup the players
+        //Player position will be sent by the game class to this when the roll happens
+        //playermovement();
       }
 
-    public void datatransfer()
-      {
+    public void datatransfer(){
         for (i = 0; i <= numplayers; i++)
           {
             name[i] = it.player[i][0];
@@ -43,12 +43,11 @@ public class playerData
           }
       }
     
-    public void playermovement()
-      {
-        dice=di.roll;//Please Add a variable which stores the roll in the dice class
-        chance=Board.chance; //This is a variable in Board which Kiska chance hai woh and this variable would be used as array index in everything
-        //If u dont get this part message me on FB
-        
-      }
+//    public void playermovement(){//Dont think we need it here as we will use it in the game class.
+//        dice= Dice.roll;//Please Add a variable which stores the roll in the dice class
+//        chance=Board.chance; //This is a variable in Board which Kiska chance hai woh and this variable would be used as array index in everything
+//        //If u dont get this part message me on FB
+//        
+//      }
 
   }
