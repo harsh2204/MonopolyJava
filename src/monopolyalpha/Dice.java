c9a25a31551d076471eac86a7d48681b388e2abc
KingKP07 <karmit1998@yahoo.com>
2017-01-01 17:14:41 -0500

    Smaill Update in Dice display Signed-off-by:KingKP07 <karmit1998@yahoo.com>

--- src/monopolyalpha/Dice.java
+++ src/monopolyalpha/Dice.java
@@ -18,15 +18,18 @@ import javax.swing.JLabel;
  *
  * @author 714449
  */
-public class Dice {
+public class Dice
+  {
 
-    public static void sleep(int amt) {
+    public static void sleep(int amt)
+      {
         long a = System.currentTimeMillis();
         long b = System.currentTimeMillis();
-        while ((b - a) <= amt) {
+        while ((b - a) <= amt)
+          {
             b = System.currentTimeMillis();
-        }
-    }
+          }
+      }
 //REDUNDANT CUZ DOESNT WORK, MAYBE FIX LATER \/
 //    private static void blinkingDie(JDialog j) {
 //        for (int i = 0; i < 10; i++) {
@@ -37,11 +40,13 @@ public class Dice {
 //        j.setVisible(false);
 //    }
 
-    public static int rollDice(int num) {
+    public static int rollDice(int num)
+      {
         int roll = 0;
         String path = null;
         String theme = InitTest.theme;
-        switch (theme) {
+        switch (theme)
+          {
             case "Generic":
                 path = "Icons/Dice/Generic/Dice ";
                 break;
@@ -51,8 +56,9 @@ public class Dice {
             case "Super":
                 path = "Icons/Dice/Superhero/Dice ";
                 break;
-        }
-        if (num == 1) {
+          }
+        if (num == 1)
+          {
             final JDialog dice = new JDialog();
             dice.setAlwaysOnTop(true);
             JLabel di = new JLabel(new ImageIcon(path + "1.png"));
@@ -65,29 +71,34 @@ public class Dice {
             dice.pack();
             dice.setLocationRelativeTo(null);
             dice.setVisible(true);
-            for (int i = 0; i < 15; i++) {
+            for (int i = 0; i < 15; i++)
+              {
                 dice.setVisible(false);
-                sleep(100);
+//                sleep(100);
                 Random r = new Random();
                 int randomNumber = r.nextInt(6) + 1;
                 di.setIcon(new ImageIcon(path + randomNumber + ".png"));
                 dice.setVisible(true);
                 di.repaint();
-                if (i == 14) {
+                if (i == 14)
+                  {
                     roll = randomNumber;
-                }
-                sleep(50);
-            }
+                  }
+                sleep(250);
+              }
 //            blinkingDie(dice);
-            di.addMouseListener(new MouseAdapter() {
-                public void mouseClicked(MouseEvent e) {
+            di.addMouseListener(new MouseAdapter()
+              {
+                public void mouseClicked(MouseEvent e)
+                  {
                     dice.setVisible(false);
-                }
-            });
+                  }
+              });
             sleep(3000);
             dice.setVisible(false);
-        }
-        if (num == 2) {
+          }
+        if (num == 2)
+          {
             final JDialog dice = new JDialog();
             dice.setAlwaysOnTop(true);
             JLabel di1 = new JLabel(new ImageIcon(path + "1.png"));
@@ -105,10 +116,10 @@ public class Dice {
             dice.setLocationRelativeTo(null);
 
             dice.setVisible(true);
-            for (int i = 0; i < 15; i++) {
+            for (int i = 0; i < 15; i++)
+              {
                 dice.setEnabled(false);
                 dice.setVisible(false);
-                sleep(100);
                 Random r1 = new Random();
                 Random r2 = new Random();
                 int randomNumber1 = r1.nextInt(6) + 1;
@@ -118,26 +129,76 @@ public class Dice {
                 dice.setVisible(true);
                 di1.repaint();
                 di2.repaint();
-                if (i == 14) {
+                if (i == 14)
+                  {
                     roll = randomNumber1 + randomNumber2;
-                }
-                sleep(50);
-            }
+                  }
+                sleep(250);
+              }
 //            blinkingDie(dice);            
-            di1.addMouseListener(new MouseAdapter() {
-                public void mouseClicked(MouseEvent e) {
+            di1.addMouseListener(new MouseAdapter()
+              {
+                @Override
+                public void mouseClicked(MouseEvent e)
+                  {
                     dice.setVisible(false);
-                }
-            });
-            di2.addMouseListener(new MouseAdapter() {
-                public void mouseClicked(MouseEvent e) {
+                  }
+              });
+            di2.addMouseListener(new MouseAdapter()
+              {
+                @Override
+                public void mouseClicked(MouseEvent e)
+                  {
                     dice.setVisible(false);
-                }
-            });
+                  }
+              });
             sleep(1500);
             dice.setVisible(false);
-        }
+          }
 
         return roll;
-    }
-}
+      }
+
+    public static void main(String args[])
+      {
+
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try
+          {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels())
+              {
+                if ("Nimbus".equals(info.getName()))
+                  {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                  }
+              }
+          } catch (ClassNotFoundException ex)
+          {
+            java.util.logging.Logger.getLogger(InitTest.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+          } catch (InstantiationException ex)
+          {
+            java.util.logging.Logger.getLogger(InitTest.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+          } catch (IllegalAccessException ex)
+          {
+            java.util.logging.Logger.getLogger(InitTest.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+          } catch (javax.swing.UnsupportedLookAndFeelException ex)
+          {
+            java.util.logging.Logger.getLogger(InitTest.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+          }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable()
+          {
+            public void run()
+              {
+                new StartScreenfrm().setVisible(true);
+              }
+          });
+      }
+  }
